e5138fa37f0955308c9aad33f28038277f634876
Joe.Kalb <kalb.joseph@gmail.com>
2019-01-01 16:35:45 -0800

    small code cleanup

---
+++ Client.js
@@ -0,0 +1,308 @@
+const tmi = require('tmi.js')
+let hangman = require('./Hangman')
+let giveaway = require('./Giveaway') 
+let randNum = require('./RandNumber')
+let quidditch = require('./Quidditch')
+let wizardDuel = require('./WizardDuel')
+const fs = require('fs');
+const houses = require('./Houses')
+let CONFIG; 
+
+if(process.env.OAUTH === undefined) {
+  let localConfig = require('./config.js')
+  CONFIG = localConfig.CONFIG;
+}
+
+// create file for logging
+let today = new Date();
+let fileName = `logs/${today.getUTCMonth()+1}-${today.getUTCDate()}.txt`;
+
+function recordPayouts(message){
+  fs.appendFile(fileName, `\n${message}`, (err) => {
+    if(err) throw err;
+    console.log(`Saved message: ${message}`)
+  })
+}
+function recordHousePoints(message){
+  fs.appendFile(fileName, ` | ${message}`, (err) => {
+    if(err) throw err;
+    console.log(`Saved message: ${message}`)
+  })
+}
+
+// Valid commands start with:
+let commandPrefix = '!'
+
+// Define configuration options:
+let opts = {
+   identity: {
+    username: 'JoeFish5',
+    password: CONFIG.OAUTH
+   },
+   channels: [
+    'thabuttress',
+    'joefish5',
+    'oooskittles',
+    'thethingssheplays'
+   ]
+}
+
+// Function called when the "dice" command is issued:
+function rollDice () {
+  const sides = 6;
+  return Math.floor(Math.random() * sides) + 1
+}
+
+// Create a client with our options:
+let client = new tmi.client(opts)
+
+client.getChannels = () => {
+  return opts.channels
+}
+
+// Register our event handlers (defined below):
+client.on('message', onMessageHandler)
+client.on('connected', onConnectedHandler)
+client.on('disconnected', onDisconnectedHandler)
+
+// Connect to Twitch:
+client.connect()
+
+// Called every time a message comes in:
+function onMessageHandler (target, context, msg, self) {
+  if (self) { return } // Ignore messages from the bot
+  // This isn't a command since it has no prefix:
+
+  // hangman logic
+  if(!hangman.getPause() && target == '#' + hangman.channel){
+    if (hangman.isAnswer(msg)) {
+      hangman.winner = context['display-name']
+      recordPayouts(`The winner is ${hangman.winner}! ${hangman.answer}`)
+      client.say(target, `The winner is ${hangman.winner}! ${hangman.answer}`)
+    }
+    else if (msg.length == 1){
+      let letter = msg.toUpperCase();
+      let numCount = hangman.isNewLetter(letter)
+      if(numCount > 0){
+        client.say(target, `${letter} appears ${numCount} time${(numCount > 1)? 's': ''}!`)
+        hangman.updateDisplay(letter)
+        if(hangman.isDisplayAnswer()){
+          hangman.winner = context['display-name']
+          recordPayouts(`The winner is ${hangman.winner}! ${hangman.answer}`)
+          client.say(target, `The winner is ${hangman.winner}! ${hangman.answer}`)
+        }
+        else client.say(target, `${hangman.display}`)
+      }else if(numCount == 0)
+        client.say(target, `no ${letter}`)
+    }
+
+    // display winner points if they are enrolled
+    if (hangman.found && houses.isEnrolled(context.username)){
+      client.say(target, `100 points to ${houses.houseNames[houses.students[context.username]]}!`)
+      recordHousePoints(`100 points to ${houses.houseNames[houses.students[context.username]]}!`)
+    }
+  }
+
+  // giveaway logic
+  if(giveaway.allowEntries && target == "#" + giveaway.channel){
+    console.log(`Checking name: ${context['display-name']}`)
+    giveaway.isNewName(context['display-name'])
+  }
+
+  // randNum logic
+  if(randNum.allowGuesses && target == "#" + randNum.channel){
+    if(randNum.guess(msg)){
+      recordPayouts(`${context['display-name']} wins! The correct number was ${randNum.number}`)
+      client.say(target, `${context['display-name']} wins! The correct number was ${randNum.number}`)
+      if(houses.isEnrolled(context.username)){
+        client.say(target, `100 points to ${houses.houseNames[houses.students[context.username]]}!`)
+        recordHousePoints(`100 points to ${houses.houseNames[houses.students[context.username]]}!`)
+      }
+    }
+  }
+
+  
+  // wizard duel logic
+  if(wizardDuel.allowEntries 
+    && target == "#" + wizardDuel.channel
+    && houses.isEnrolled(context.username)
+    && !wizardDuel.checkIfInDuel(context['display-name'])){
+      wizardDuel.enter(context['display-name'], houses.students[context.username])
+      console.log(`${context['display-name']} entered the Dueling Club`)
+  }
+
+  // all commands go under here!
+  if (msg.substr(0, 1) !== commandPrefix) {
+    // this shows all of chat
+    //console.log(`[${target} (${context['message-type']})] ${context['display-name']}: ${msg}`)
+    return
+  }
+  // Split the message into individual words:
+  const parse = msg.slice(1).split(' ')
+  // The command name is the first (0th) one:
+  const commandName = parse[0]
+  
+
+  // switch cases for wizardDuel only
+  if(wizardDuel.beginDuel || wizardDuel.allowBets 
+    || wizardDuel.allowEntries || wizardDuel.studentCount){
+    switch(commandName){ // 
+      case 'duel':
+      // joining the duel will be here
+      if(wizardDuel.allowEntries
+        && target == "#" + wizardDuel.channel){
+        if(!houses.isEnrolled(context.username))
+          client.say(target, `Sorry ${context['display-name']}, you don't have a !house yet.`)
+        else{
+          if(wizardDuel.checkIfInDuel(context['display-name']))
+            client.say(target, `${context['display-name']} is in the Dueling Club!`)
+          else{
+            wizardDuel.enter(context['display-name'], houses.students[context.username])
+            client.say(target, `Welcome to the dueling club ${context['display-name']}!`)
+          }
+        }
+      }
+      if(wizardDuel.winnerFound
+        && target == "#" + wizardDuel.channel
+        && wizardDuel.checkIfInDuel(context['display-name']))
+        client.say(target, wizardDuel.myResults(context['display-name']))
+        break;
+      case 'pickDuelists':
+        if(target == "#" + wizardDuel.channel
+        && (context.username == "joefish5" || context.username == "thabuttress")){
+          client.say(target, wizardDuel.pickDuelists())
+        }
+        break;
+      case 'bet':
+        if(target == "#" + wizardDuel.channel
+          && wizardDuel.allowBets
+          && wizardDuel.checkIfInDuel(context['display-name'])){
+          let arrMsg = msg.split(' ');
+          let readyToDuel = false 
+          if(arrMsg[1])
+            readyToDuel = wizardDuel.placeBet(context['display-name'], arrMsg[1])
+          else
+            client.say(target, `Sorry ${context['display-name']}, need to do !bet 1 or !bet 2`)
+          if(readyToDuel)
+            client.say(target, "All bets have been placed!")
+        }
+        else if(target == "#" + wizardDuel.channel
+          && wizardDuel.winnerFound
+          && wizardDuel.checkIfInDuel(context['display-name'])){
+            client.say(target, wizardDuel.myResults(context['display-name']))
+          }
+        else if(target == "#" + wizardDuel.channel
+          && !wizardDuel.allowBets){
+            client.say(target, `Sorry ${context['display-name']}. There's no one to !bet on yet.`)
+          }
+        break;
+      case 'duelHouses':
+        if(target == "#" + wizardDuel.channel)
+          client.say(target, wizardDuel.showEntries())
+        break;
+      default:
+        // commands during wizard duel that do not apply
+        //console.log(`Wizard Duel Switch Case Default: ${commandName}`)
+    }
+  }
+  // If the command is known, let's execute it:
+  switch(commandName){
+    case 'dice':
+      const num = rollDice()
+      client.say(target, `You rolled a ${num}`)
+      break;
+    case 'nerds':
+      if(target == "#joefish5")
+        client.say(target, "You're all such nerds!!!")
+      console.log(`* Executed ${commandName} command`)
+      break;
+    case 'hangman':
+      if(target == "#" + hangman.channel){
+        (!hangman.getPause()) 
+          ? client.say(target, `${hangman.display}`) 
+          : client.say(target, "Hangman is currently paused!")
+      }
+      break;
+    case 'guessed':
+      if(!hangman.getPause() && target == "#" + hangman.channel)
+        client.say(target, `Letters already guessed: ${hangman.alreadyGuessed()}`)
+      break;
+    case 'start':
+      if(context['display-name'] == "JoeFish5" && target == "#" + giveaway.channel){
+        giveaway.start(target.replace("#", ""))
+        client.say(giveaway.channel, "GIVEAWAY HAS STARTED! Talk in chat to enter!")
+      }
+      break;
+    case 'me':
+      if(giveaway.allowEntries && target == "#" + giveaway.channel)
+      client.say(giveaway.channel, 
+        `${context['display-name']} is${(giveaway.check(context['display-name']) 
+        ? " ": " not ")}in the giveaway. There's current ${giveaway.count} enteries.`)
+      break;
+    case 'play':
+      if(quidditch.gameOn && target == "#" + quidditch.channel){
+        let play = quidditch.play(context['display-name'])
+        if(play == 10){
+          client.say(quidditch.channel, `${context['display-name']} threw the Quaffle and scored! That's 10 points buttOMG 
+            ${(quidditch.users[context['display-name']].tries < quidditch.maxTries)? quidditch.maxTries-quidditch.users[context['display-name']].tries : "No"} 
+            ${(quidditch.users[context['display-name']].tries == quidditch.maxTries-1) ? "try" : "tries"} left.`)
+        } else if(play == 0){
+          client.say(quidditch.channel, `${context['display-name']} threw the Quaffle and missed! buttThump 
+            ${(quidditch.users[context['display-name']].tries < quidditch.maxTries)? quidditch.maxTries-quidditch.users[context['display-name']].tries : "No"}
+            ${(quidditch.users[context['display-name']].tries == quidditch.maxTries-1) ? "try" : "tries"} left.`)
+        }else{
+          console.log(`${context['display-name']} has hit the max tries of ${quidditch.users[context['display-name']].tries}`)
+        }
+      }
+      break;
+    case 'results':
+      if(!quidditch.gameOn && quidditch.playerCount && target == "#" + quidditch.channel){
+        client.say(quidditch.channel, quidditch.finalPayouts())
+      }
+      break;
+    case 'snitch':
+      if(!quidditch.gameOn && quidditch.playerCount 
+        && target == "#"+quidditch.channel){
+        client.say(quidditch.channel, `${quidditch.snitch} caught the snitch!`)
+      }
+      break;
+    case 'mypoints':
+      if(target == "#" + quidditch.channel
+        && quidditch.users[context['display-name']])
+        client.say(target, quidditch.myPoints(context['display-name']))
+
+      break;
+    case 'whathouse':
+      if(target == "#thabuttress" || target == "#joefish5"){
+        let tryName = msg.split(' ')[1];
+        (!tryName) ? client.say(target, houses.getHouse(context['display-name']))
+          : client.say(target, houses.getHouse(tryName))
+      }
+      break;
+    case 'houses':
+      if(target == "#thabuttress" || target =="#joefish5"){
+        client.say(target, houses.classSizes())
+      }
+      break;
+    case 'myhouse':
+      if(target == "#thabuttress" || target == "#joefish5"){
+        client.say(target, houses.myHouse(context['display-name']))
+      }
+      break;
+    default:
+      // this shows unknows commands
+      //console.log(`* Unknown command ${commandName}`)
+  }
+}
+
+function onConnectedHandler (addr, port) {
+  console.log(`* Connected to ${addr}:${port}`)
+}
+
+// Called every time the bot disconnects from Twitch:
+function onDisconnectedHandler (reason) {
+  console.log(`Disconnected: ${reason}`)
+  process.exit(1)
+}
+
+module.exports = client
---
+++ Routes.js
@@ -0,0 +1,213 @@
+const express = require('express')
+let router = express.Router()
+
+let hangman = require('./Hangman')
+let giveaway = require('./Giveaway') 
+let randNum = require('./RandNumber')
+let quidditch = require('./Quidditch')
+let wizardDuel = require('./WizardDuel')
+
+const fs = require('fs');
+let today = new Date();
+let fileName = `logs/${today.getUTCMonth()+1}-${today.getUTCDate()}.txt`;
+
+function recordPayouts(message){
+  fs.appendFile(fileName, `\n${message}`, (err) => {
+    if(err) throw err;
+    console.log(`Saved message: ${message}`)
+  })
+}
+
+function test(target){
+  console.log(`Testing in channel: ${target}`)
+  client.say(target, "This is a test!")
+}
+
+let client = require('./Client')
+
+router.get('/channels', (req, res) => {
+  res.status(200).json(client.getChannels())
+})
+
+router.get('/testBtn', (req, res) => {
+  console.log("here for some reason")
+  res.status(404).json("here for some reason")
+})
+
+router.get('/testBtn/:channel', (req, res) => {
+  try{
+    test(req.params.channel)
+    res.status(200).json("Test Worked")
+  }
+  catch (err){
+    console.log(err)
+    res.status(404).send(err)
+  }
+})
+
+function delayedWinnings(target, messages){
+  for(let i in messages){
+    setTimeout(() => {
+      client.action(target, messages[i])   
+    }, 3000 + i*3000)
+  }
+}
+
+// hangman
+router.post('/hangman', (req, res) => {
+  try{
+    hangman.start(req.body)
+    client.say(hangman.channel, `It's time for some hangman! ${hangman.wordCount} words (!hangman !guesses)`)
+    client.say(hangman.channel, `${hangman.display}`)
+    res.status(200).json(hangman.answer)
+  }
+  catch(err){
+    res.status(404).send(err)
+  }
+})
+
+router.get('/hangman/pause', (req, res) => {
+  (hangman.getPause()) ? hangman.pause = false  
+    : hangman.pause = true;
+  
+  res.status(200).json(hangman.getPause())
+})
+
+router.get('/hangman/clear', (req, res) => {
+  hangman.clear();
+  res.status(200).json("hangman reset")
+})
+
+//giveaway
+router.get('/giveaway/:channel', (req, res) => {
+  giveaway.start(req.params.channel)
+  client.say(giveaway.channel, "GIVEAWAY HAS STARTED! Talk in chat to enter!")
+  res.status(200).json("Giveaway Started")
+})
+
+router.get('/giveaway/game/stop', (req, res) => {
+  giveaway.stopEntries()
+  client.say(giveaway.channel, "Giveaway is now closed!")
+  res.status(200).json("No More Entries")
+})
+
+router.get('/giveaway/game/draw', (req, res) => {
+  let winner = giveaway.drawWinner()
+  client.say(giveaway.channel, `Out of ${giveaway.count + 1} people the winner is ${winner}!`)
+  res.status(200).json(`This winner is ${winner}`)
+})
+
+router.get('/giveaway/game/clear', (req, res) => {
+  giveaway.clear()
+  res.status(200).json("Giveaway now Cleared")
+})
+
+// rand number calls
+router.get('/randNum/:info', (req, res) => {
+  let info = req.params.info.split("+")
+  randNum.start(info[0], info[1])
+  client.say(info[1], `Guess a number betweet 1 and ${info[0]}`)
+  res.status(200).json(`The correct number is ${randNum.number}`)
+})
+
+router.get('/randNum/game/clear', (req, res) => {
+  randNum.clear()
+  console.log("RANDOM NUMBER WAS CLEARED!!!")
+  res.status(200).json("Clear Random Number")
+})
+
+// quidditch endpoints
+router.get('/quidditch/:channel', (req, res) => {
+  quidditch.start(req.params.channel)
+  client.say(req.params.channel, "Want to play some Quidditch! Do !play to join the game!!!")
+  res.status(200).json("Quidditch Game Started")
+})
+
+router.get('/quidditch/game/over', (req, res) => {
+  if(quidditch.playerCount){
+    let snitch = quidditch.snitchCaught();
+    let winnings = quidditch.finalPayouts();
+    let messages = [`${snitch} caught the Golden Snitch and ended the game!`, winnings]
+    delayedWinnings(quidditch.channel, messages)
+    recordPayouts(`${winnings} | ${snitch} caught the snitch!`)
+    res.status(200).json(`${snitch} caught the snitch!`)
+  } 
+  else res.status(200).json("No one played")
+})
+
+router.get('/quidditch/game/clear', (req, res) => {
+  quidditch.clear();
+  res.status(200).json("Quidditch Game Reset");
+})
+
+router.get('/quidditch/game/results', (req, res) => {
+  if(!quidditch.gameOn && quidditch.playerCount){
+    client.say(quidditch.channel, quidditch.finalPayouts())
+  }
+  res.status(200).json("Quidditch Results Posted")
+})
+
+router.get('/quidditch/game/payout', (req, res) => {
+  if(quidditch.channel == 'thabuttress'){
+    let allPlays = Object.keys(quidditch.users);
+    for(let i = 0; i < quidditch.playerCount; ++i){
+      if(quidditch.users[allPlays[i]].points)
+        client.say(quidditch.channel, `!buttcoins add ${allPlays[i]} ${quidditch.users[allPlays[i]].points}`)
+    }
+  }
+  res.status(200).json("Payouts done!")
+})
+
+// wizard duel calls
+router.get('/duel/:channel', (req, res) => {
+  wizardDuel.start(req.params.channel);
+  client.say(req.params.channel, `Want to join the duel club? Just keep talking in chat and as long as you're in a 
+    !house you might get a chance to prove yourself to your peers! [!wizard !duel]`)
+  res.status(200).json("The Dueling Club is open!")
+})
+
+router.get('/duel/game/pick', (req, res) =>{
+  if(wizardDuel.studentCount > 2){
+    client.action("#" + wizardDuel.channel, wizardDuel.pickDuelists());
+    res.status(200).json(`${wizardDuel.duelists[1]} VS ${wizardDuel.duelists[2]}`)
+  }else
+    res.status(200).json("Not enough students")
+})
+
+router.get('/duel/game/start', (req, res) => {
+  if(wizardDuel.allowBets){
+    client.say("#"+wizardDuel.channel, wizardDuel.readyToDuel());
+    wizardDuel.timeToDuel();
+    wizardDuel.finalHousePayouts();
+
+    let actions = []
+    let results = wizardDuel.houseResults();
+    actions = wizardDuel.duelArray();
+    actions.push(wizardDuel.champ());
+    actions.push(results);
+
+    delayedWinnings(wizardDuel.channel, actions)
+   
+    recordPayouts(results)
+    res.status(200).json(wizardDuel.champ())
+  }
+  else res.status(200).json("Bets aren't in yet")
+})
+
+router.get('/duel/game/clear', (req, res) => {
+  wizardDuel.clear();
+  res.status(200).json("clearing the duel")
+})
+
+// clear all info
+router.get('/clear/all', (req, res) => {
+  giveaway.clear();
+  hangman.clear();
+  quidditch.clear();
+  randNum.clear();
+  wizardDuel.clear();
+  res.status(200).json("All Games Cleared!")
+})
+
+
+module.exports = router
