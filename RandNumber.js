46ec9062583f224fa34d8545645d88ef7f1f3cf1
Joseph Kalb <kalb.joseph@gmail.com>
2019-02-06 12:12:59 -0800

    making a class with constructor

--- RandNumber.js
+++ RandNumber.js
@@ -3,22 +3,46 @@ module.exports = {
   upper: 0,
   allowGuesses: false,
   channel: "",
-  start: function(upper, channel){
+  start (upper, channel){
     this.number = Math.floor(Math.random() * upper) + 1;
     this.channel = channel
     this.allowGuesses = true
   },
-  guess: function(num){
+  guess(num){
     if(num == this.number){
       this.allowGuesses = false;
       return true
     }
     return false
   },
-  clear: function(){
+  clear(){
     this.number = 0;
     this.upper = 0;
     this.channel = ""
     this.allowGuesses = false;
   }
 }
+
+/* class RandNum {
+  contructor(upper, channel){
+    this.upper = upper
+    this.channel = channel
+    this.number = Math.floor(Math.random() * upper) + 1
+    this.allowGuesses = true;
+  }
+
+  guess(num){
+    if(num == this.number){
+      this.allowGuesses = false;
+      return true;
+    }
+    return false;
+  }
+
+  clear() {
+    this.upper = 0;
+    this.channel = ""
+    this.number = 0
+    this.allowGuesses = false
+  }
+} */
