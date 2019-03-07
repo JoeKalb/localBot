b7bedca9cc697011ac3a693b30fcd559968bd708
Joseph Kalb <kalb.joseph@gmail.com>
2019-03-07 11:48:03 -0800

    house change

--- Houses.js
+++ Houses.js
@@ -182,7 +182,8 @@ module.exports = {
     eyequeuex:3,
     mysteracles:2,
     thedrummergirl:1,
-    cloudsavegaming:1
+    cloudsavegaming:1,
+    crelca:0
     //gryf = 0, huff = 1, sly = 2, raven = 3
   },
   getHouse: function(name){
