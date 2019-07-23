aee155b4d73e593a80a92c871b28f3772e053cc2
Joseph Kalb <kalb.joseph@gmail.com>
2019-07-23 11:22:00 -0700

    added bot commands

--- ttsp.js
+++ ttsp.js
@@ -10,14 +10,62 @@ const response = {
     items:[]
 }
 
+let backup = false;
+
 module.exports = {
     channel:"#thethingssheplays",
     handelMessage:(context, msg) => {
         let result = Object.assign({}, response)
         result.items = []
+        
+        if((context.mod || context.username === 'thethingssheplays') 
+            && msg === '!backup'){
+            (backup) ? backup = false : backup = true;
+            result.hasMessage = true;
+            result.items = [
+                `Backup commands are now ${(backup) ? 'on':'off'}.`
+            ]
+            return result;
+        }
 
-        //console.log(context)
-        //console.log(`Message: ${msg}`)
+        if(msg.substr(0,1) !== commandPrefix || !backup)
+            return result;
+
+        const parse = msg.slice(1).split(' ')
+        const commandName = parse[0]
+
+        switch(commandName){
+            case 'discord':
+                result.hasMessage = true;
+                result.items = [`Come hang out in the discord! https://discordapp.com/invite/jpR8f6n`]
+                break;
+            case 'twitter':
+                result.hasMessage = true;
+                result.items = [`Tweetar: https://twitter.com/ThingsSheTweets`]
+                break;
+            case 'insta':
+                result.hasMessage = true;
+                result.items = [`Insta: https://www.instagram.com/thingsshedraws/`]
+                break;
+            case 'instagram':
+                result.hasMessage = true;
+                result.items = [`Insta: https://www.instagram.com/thingsshedraws/`]
+                break;
+            case 'yt':
+                result.hasMessage = true;
+                result.items = [`Sara's YouTube: https://www.youtube.com/c/TheThingsShePlays`]
+                break;
+            case 'youtube':
+                result.hasMessage = true;
+                result.items = [`Sara's YouTube: https://www.youtube.com/c/TheThingsShePlays`]
+                break;
+            case 'merch':
+                result.hasMessage = true;
+                result.items = [`Wanna rep some Thump Nation Merch! https://www.designbyhumans.com/shop/TheThingsShePlays/`]
+                break;
+            default:
+        }
+
         return result;
     },
     cheerHandler:(userstate, msg) => {
