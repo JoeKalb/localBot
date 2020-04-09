359007957d5d16235ae3aa185484c3789d08d63a
Joseph Kalb <kalb.joseph@gmail.com>
2020-04-08 23:09:07 -0700

    removed emotes

--- thaButtress.js
+++ thaButtress.js
@@ -672,6 +672,8 @@ module.exports = {
     },
     subHandler: (username, method, message, userstate) => {
         let result = Object.assign({}, response)
+
+        return result // remove later
         if(username === 'joefish5') return result;
         
         result.hasMessage = true;
@@ -695,7 +697,7 @@ module.exports = {
             result.hasPayout = true;
 
             result.items = [
-                'buttHella buttHype buttHella buttHype buttHella buttHype buttHella buttHype buttHella buttHype buttHella buttHype',
+                /*'buttHella buttHype buttHella buttHype buttHella buttHype buttHella buttHype buttHella buttHype buttHella buttHype',*/
                 singleGiftPayout(username, methods)
             ]
         } 
@@ -713,8 +715,8 @@ module.exports = {
 
         let hype = 'buttHella buttHype '
         result.items = [
-            (numbOfSubs > 13) ? hype.repeat(Math.floor(numbOfSubs/2)) : 
-            'buttHella buttHype buttHella buttHype buttHella buttHype buttHella buttHype buttHella buttHype buttHella buttHype',
+            /*(numbOfSubs > 13) ? hype.repeat(Math.floor(numbOfSubs/2)) : 
+            'buttHella buttHype buttHella buttHype buttHella buttHype buttHella buttHype buttHella buttHype buttHella buttHype',*/
             multiGiftPayout(username, numbOfSubs, methods)
         ]
         return result;
